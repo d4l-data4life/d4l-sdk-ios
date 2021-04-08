f65df0899847dea358be813b42eb131e79985963
Alessio Borraccino <alessio.borraccino@data4life.care>
2021-04-08 15:56:23 +0200

    Updates package dependencies

--- Package.swift
+++ Package.swift
@@ -27,7 +27,8 @@ let package = Package(
                  .upToNextMinor(from: "5.4.1")),
         .package(url: "https://github.com/freshOS/Then",
                  .upToNextMinor(from: "5.1.4")),
-        .package(url: "https://github.com/AppAuth/AppAuth-iOS.git",
+        .package(name: "AppAuth",
+                 url: "https://github.com/openid/AppAuth-iOS.git",
                  .upToNextMinor(from: "1.4.0"))
     ],
     targets: [
@@ -36,28 +37,42 @@ let package = Package(
         .binaryTarget(
             name: "Data4LifeSDK",
             url: "https://github.com/d4l-data4life/d4l-sdk-ios/releases/download/1.13.0/Data4LifeSDK-xcframework-1.13.0.zip",
-            checksum: "287e39f125dd9559c55721e1ea6fb354a1a6f18808cf7ff929d9f1289bdc401c"
+            checksum: "498c58323b8d752eac3b7184a0327fe8e7bdc5587bf3bc15de241500b599d991"
         ),
         .binaryTarget(
             name: "Data4LifeCrypto",
             url: "https://github.com/d4l-data4life/d4l-fhir-ios/releases/download/1.13.0/Data4LifeCrypto-xcframework-1.5.0.zip",
-            checksum: "e650e60d9e1c2c929c31b363caf23f4e01526ec38173e36409b7adff5711caa3"
+            checksum: "e97a5548952d616bbe8cf609ff767da108a2021586c7c05807d7886e770989a7"
         ),
         .target(name: "Data4LifeSDKFrameworks",
                 dependencies: [
-                    .product(name: "Data4LifeSDK",
-                             package: "Data4LifeSDK",
+                    .product(name: "CryptoSwift",
+                             package: "Data4LifeSDKUtils",
                              condition: .when(platforms: [.iOS])),
-                    .product(name: "Data4LifeCrypto",
-                             package: "Data4LifeCrypto",
+                    .product(name: "Data4LifeSDKUtils",
+                             package: "Data4LifeSDKUtils",
                              condition: .when(platforms: [.iOS])),
-                    .target(name: "Data4LifeSDK")
+                    .product(name: "Data4LifeFHIR",
+                             package: "Data4LifeFHIR",
+                             condition: .when(platforms: [.iOS])),
+                    .product(name: "ModelsR4",
+                             package: "Data4LifeFHIR",
+                             condition: .when(platforms: [.iOS])),
+                    .product(name: "Then",
+                             package: "Then",
+                             condition: .when(platforms: [.iOS])),
+                    .product(name: "Alamofire",
+                             package: "Alamofire",
+                             condition: .when(platforms: [.iOS])),
+                    .byName(name: "AppAuth"),
+                    .target(name: "Data4LifeSDK"),
+                    .target(name: "Data4LifeCrypto")
                 ],
                 path: "SDKSPMFrameworks"),
         .testTarget(name: "Data4LifeSDKTests",
                     dependencies: ["Data4LifeSDKFrameworks"],
                     path: "SDK/Tests",
-                    exclude: ["Info.plist"],
-                    resources: [.process("Resources")])
+                    exclude: ["Info.plist","Data4LifeSDK-Version.plist"],
+                    resources: [.process("Resources"),.process("BenchmarkClient/SubbedResponses")])
     ]
 )
