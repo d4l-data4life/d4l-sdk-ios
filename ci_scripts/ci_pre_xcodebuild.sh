8c4c1410c1c320664fe0c85fb615733e35e16287
Alessio Borraccino <alessio.borraccino@data4life.care>
2022-01-14 16:02:40 +0100

    Change to workspace

--- ci_scripts/ci_pre_xcodebuild.sh
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -6,6 +6,6 @@
 #  Created by Alessio Borraccino on 14.01.22.
 #  Copyright © 2022 HPS Gesundheitscloud gGmbH. All rights reserved.
 
-cd $CI_PROJECT_FILE_PATH
+cd $CI_WORKSPACE
 printf '%s\n' "$D4L_EXAMPLE_CONFIG_JSON" > d4l-example-app-config.json
 ./config-generator.swift d4l staging
